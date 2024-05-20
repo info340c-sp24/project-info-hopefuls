0e516c090f418e1eb422e59ffdb45ba9d592eda4
jesiah <jesiah@uw.edu>
2024-05-19 23:15:39 -0700

    Some finishing touches

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,12 +3,8 @@ import React from 'react';
 import ReactDOM from "react-dom";
 import Signin from './Signin';
 import Landing from './Landing';
-<<<<<<< HEAD
-import Schedule from './schdeule';
-=======
 import Createwo from './createwo';
 import SearchWorkouts from './search';
->>>>>>> 8e8260afb4526bd28c1929f2d8f1ff7acdf5c2a5
 import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 
 function App() {
@@ -17,12 +13,8 @@ function App() {
       <Routes>
         <Route path = "/" element={<Landing></Landing>}></Route>
         <Route path = "/signin" element={<Signin></Signin>}></Route>
-<<<<<<< HEAD
-        <Route path = "/schdeule" element={<Schedule></Schedule>}></Route>
-=======
         <Route path = "/Create_page.html" element={<Createwo></Createwo>}></Route>
         <Route path = "/search.html" element={<SearchWorkouts />}></Route>
->>>>>>> 8e8260afb4526bd28c1929f2d8f1ff7acdf5c2a5
       </Routes>
     </Router>
   );
