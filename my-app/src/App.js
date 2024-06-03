da6e918d6496ff697372448f9c6842879c38dc63
Sofila Song <feiyas@uw.edu>
2024-06-03 01:01:40 -0700

    SearchWorkout is working

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from "react-dom";
 import Signin from './Signin';
 import Landing from './Landing';
-import Createwo from './Createwo';
+import Createwo from './createwo';
 import SearchWorkouts from './search';
 import Schedule from './schedule';
 import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
